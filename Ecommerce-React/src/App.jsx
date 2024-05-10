bf3983a86b1303f4ed04ba4b2074fdc2b1572030
MohammedIzhar77 <izharmohammed21@gmail.com>
2024-05-10 11:43:43 +0530

    configured react strap

--- Ecommerce-React/src/App.jsx
+++ Ecommerce-React/src/App.jsx
@@ -2,14 +2,42 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import { Nav, NavItem,NavLink   } from 'reactstrap';
 function App() {
 
 
   return (
-<>
-Shopcart - React
-</>
+<Nav
+  fill
+  pills
+>
+  <NavItem>
+    <NavLink
+      active
+      href="#"
+    >
+      Link
+    </NavLink>
+  </NavItem>
+  <NavItem>
+    <NavLink href="#">
+      Much Longer Nav Link
+    </NavLink>
+  </NavItem>
+  <NavItem>
+    <NavLink href="#">
+      Another Link
+    </NavLink>
+  </NavItem>
+  <NavItem>
+    <NavLink
+      disabled
+      href="#"
+    >
+      Disabled Link
+    </NavLink>
+  </NavItem>
+</Nav>
   )
 }
 
